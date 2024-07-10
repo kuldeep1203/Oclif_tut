3c09798a42041bcd11b5ba7fab184d244659e91a
kuldeep1203 <kuldeeppawar427@gmail.com>
2024-07-10 11:33:02 +0530

    "ts"

--- src/commands/hello.ts
+++ src/commands/hello.ts
@@ -1,6 +1,12 @@
 const Command  = require("@oclif/core").Command;
 
 export default class Hello extends Command {
+
+    static override arg = {
+
+    }
+
+
     public async run(): Promise<void>{
         this.log("Hello from oclif ")
     }
